a6ded9282f99fb64c529edf9f24eea38e33e51ab
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-11-28 16:31:22 +0000

    added undescore and some validation

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -18,22 +20,23 @@ app.get('/todos', function (req, res) {
 
 // GET /todo/:id
 app.get('/todos/:id', function (req, res) {
-  var id = parseInt(req.params.id, 10);
-  var todoMatch;
-  todos.forEach(function (todo) {
-    if (todo.id && todo.id === id) {
-      todoMatch = todo;
-    }
-  });
+  var todoId = parseInt(req.params.id, 10);
+  var todoMatch = _.findWhere(todos, {id: todoId});
   return todoMatch ? res.json(todoMatch) : res.status(404).send();
 });
 
 // POST /todos
 app.post('/todos', function (req, res) {
   var body = req.body;
-  body.id = todoNextId++;
-  todos.push(body);
-  res.json(body);
+  var todo = _.pick(body, 'description', 'completed');
+  if (!_.isBoolean(todo.completed) || !_.isString(todo.description) || todo.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+  todo.id = todoNextId++;
+  todo.description = todo.description.trim();
+  todos.push(todo);
+  res.json(todo);
 });
 
 app.listen(PORT, function () {

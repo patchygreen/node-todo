337be7ab0c700c88085d119165d63d8289c056d9
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-11-29 14:27:31 +0000

    adda an update method

--- server.js
+++ server.js
@@ -50,6 +50,33 @@ app.delete('/todos/:id', function (req, res) {
   res.json(todos);
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var todoMatch = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+  if (!todoMatch) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+      return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  _.extend(todoMatch, validAttributes);
+  res.json(todoMatch);
+});
+
+
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
 });

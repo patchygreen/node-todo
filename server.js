d71cb5ed61d8c1c456daa6b5b38abbe623ab960b
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-11-30 16:04:47 +0000

    adding description search

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
   res.send('ToDo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
@@ -24,6 +24,11 @@ app.get('/todos', function (req, res) {
       filteredTodos = _.where(todos, {completed: false});
   }
 
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(todos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    })
+  }
   res.json(filteredTodos);
 });
 

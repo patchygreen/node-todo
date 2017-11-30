2cadd9a52140c4301f0d630147fc333064fda10c
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-11-30 15:48:15 +0000

    added completed filter

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+//   name: 'Andrew',
+//   age: 21
+// };
+//
+// function updatePerson(obj) {
+//   // obj = {
+//   //   name: 'Andrew',
+//   //   age: 24
+//   // };
+//   obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15,37];
+
+function addGrades(arr) {
+  // arr.push(22);
+  arr = [22];
+}
+
+addGrades(grades);
+console.log(grades);

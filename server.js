4771d34f4894192b97bd8e03bace14e804430975
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-12-05 14:46:03 +0000

    Update PUT /todos/:id

--- server.js
+++ server.js
@@ -84,27 +84,29 @@ app.delete('/todos/:id', function (req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var todoMatch = _.findWhere(todos, {id: todoId});
   var body = _.pick(req.body, 'description', 'completed');
-  var validAttributes = {};
-  if (!todoMatch) {
-    return res.status(404).send();
-  }
+  var attributes = {};
 
-  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    return res.status(400).send();
+  if (body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
-
-  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-    validAttributes.description = body.description;
-  } else if (body.hasOwnProperty('description')) {
-    return res.status(400).send();
+  if (body.hasOwnProperty('description') ) {
+    attributes.description = body.description;
   }
-
-  _.extend(todoMatch, validAttributes);
-  res.json(todoMatch);
+  
+  db.todo.findById(todoId).then((todo) => {
+    if (todo) {
+      todo.update(attributes).then((todo) => {
+        res.json(todo.toJSON());
+      }, (e) => {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, () => {
+    res.status(500).send();
+  });
 });
 
 db.sequelize.sync().then(function () {

b1a066917b99c9f3e762e0bcf06b9e37d90068ce
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-12-05 13:56:59 +0000

    refactor of DELETE /todo:id with sequelize

--- server.js
+++ server.js
@@ -64,12 +64,21 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var todoMatch = _.findWhere(todos, {id: todoId});
-  if (!todoMatch) {
-    res.status(404).json({"error": "no todo found with that id"});
-  }
-  todos = _.without(todos, todoMatch);
-  res.json(todos);
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then((rowsDeleted) => {
+    if (rowsDeleted === 0) {
+      res.status(404).json({
+        error: 'No todo with that id.'
+      })
+    } else {
+      res.status(204).send();
+    }
+  }, (e) => {
+    res.status(500).send;
+  });
 });
 
 // PUT /todos/:id
